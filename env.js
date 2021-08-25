d221f30bb40b94a75bd02ad0a262735105a913d8
Victor Nakasone <victorsnakasone@gmail.com>
2021-08-24 21:53:32 -0300

    Adds firebase configuration

---
+++ env.js
@@ -0,0 +1,9 @@
+export default= {
+  apiKey: "AIzaSyBdRiraR6JjXgbGiUdLsMMGIbedM8Tg-aA",
+  authDomain: "loop-garden.firebaseapp.com",
+  projectId: "loop-garden",
+  storageBucket: "loop-garden.appspot.com",
+  messagingSenderId: "665819551888",
+  appId: "1:665819551888:web:7a9612dbb0cc13dd3be015",
+  measurementId: "G-SYV41SLXQ4"
+};
